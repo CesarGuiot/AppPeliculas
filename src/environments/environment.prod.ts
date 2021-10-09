816bf75fad692c3f47ef56494dcd8e29831529ba
CESAR GUIOT <elguiot10@gmail.com>
2021-10-09 07:58:44 -0500

    Actualización Imagenes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url:'https://api.themoviedb.org/3',
+  apikey:'9aad541ae56dff302e06033fe1581f02',
+  imgPath:'https://image.tmdb.org/t/p'
 };
